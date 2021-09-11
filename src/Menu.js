27cd058bb83cb6ff1c8ed16e898b471892719ee0
JuniorUyi <juniorjerry91@gmail.com>
2021-09-11 08:34:17 +0100

    Updated the stateless components to make the responsive video work

---
+++ src/Menu.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export class Menu extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+
+    handleClick(e) {
+        const text = e.target.value;
+        this.props.chooseVideo(text);
+    }
+
+    render() {
+        return  (
+            <form onClick={this.handleClick}>
+                <input type="radio" name="src" value="fast" /> Fast
+                <input type="radio" name="src" value="slow" /> Slow
+                <input type="radio" name="src" value="cute" /> Cute
+                <input type="radio" name="src" value="eek" /> Eek
+            </form>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Video } from './Video';
+import { Menu } from './Menu';
+import fastVideo from './videos/react_video-fast.mp4';
+import slowVideo from './videos/react_video-slow.mp4';
+import cuteVideo from './videos/react_video-cute.mp4';
+import eekVideo from './videos/react_video-eek.mp4';
+
+const VIDEOS = {
+    fast: fastVideo,
+    slow: slowVideo,
+    cute: cuteVideo,
+    eek: eekVideo
+};
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { src: VIDEOS.fast};
+
+        this.chooseVideo = this.chooseVideo.bind(this);
+    }
+
+    chooseVideo(newVideo) {
+        this.setState({
+            src: VIDEOS[newVideo]
+        }) 
+    }
+
+    render() {
+        return ( 
+            <div>
+                <h1>Video Player</h1>
+                <Menu chooseVideo={this.chooseVideo}/>
+                <Video src={this.state.src}/>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
